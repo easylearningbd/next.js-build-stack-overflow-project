3bb3141843068d6863b8742476d04f3217517e52
easylearningbd <elebdsoft@gmail.com>
2025-02-15 01:11:48 +0600

    n23 Implement Github SignIn Part 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local"
 import "./globals.css";
 import ThemeProvider from "@/context/Theme"; 
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -44,6 +45,7 @@ export default function RootLayout({
       {children}
 
       </ThemeProvider> 
+      <Toaster/>
        
       </body>
     </html>
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -7,9 +7,16 @@ import Image from 'next/image';
 const SocialAuthForm = () => {
     const buttonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
 
+    const handleSignIn = async (provider: "github") => {
+        
+    }
+
+
+
+
     return (
         <div className='mt-10 flex flex-wrap gap-2.5'>
-            <Button className={buttonClass}>
+            <Button className={buttonClass} onClick={() => handleSignIn("github")}>
                 <Image
                     src="/icons/github.svg"
                     alt='Github Logo'
