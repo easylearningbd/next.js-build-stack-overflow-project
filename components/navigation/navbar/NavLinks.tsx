c016313a61146b9ec7dbf9bf2a34412a5b4640a7
easylearningbd <elebdsoft@gmail.com>
2025-02-15 16:42:47 +0600

    n28 Mobile Navigation Part 1

---
+++ components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+
+const NavLinks = () => {
+    return (
+        <div>
+            <h1>nav links here </h1>
+        </div>
+    );
+};
+
+export default NavLinks;
