1546e5d5ab2e5ea4712c462a7e82d73f93ff179f
easylearningbd <elebdsoft@gmail.com>
2025-02-16 02:44:07 +0600

    n36 Implement Right Sidebar Part 3

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,4 +1,7 @@
+import ROUTES from '@/constants/routes';
+import Link from 'next/link';
 import React from 'react';
+import { Badge } from '../ui/badge';
 
 interface Props {
     _id: string;
@@ -9,11 +12,22 @@ interface Props {
 }
 
 const TagCard = ({ _id,name,questions,showCount,compack }: Props) => {
-    
+
     return (
-        <div>
-            tags 
-        </div>
+         <Link href={ROUTES.TAGS(_id)} className='flex justify-between gap-2'>
+            <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase'>
+                <div className='flex-center space-x-2'>
+                    <span>{name}</span> 
+                </div>  
+            </Badge>
+        
+        {
+          showCount && (
+            <p className='small-medium text-dark500_light700'>{questions}</p>
+          )  
+        }
+         
+         </Link>
     );
 };
 
