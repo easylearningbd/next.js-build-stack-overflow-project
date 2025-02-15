46172a4d0e297b220c4926b3689b3009db335d40
easylearningbd <elebdsoft@gmail.com>
2025-02-16 04:16:11 +0600

    n39 Local Search Bar Part 1

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
+import { Input } from '../ui/input';
 
-const LocalSearch = () => {
+interface Props {
+    route: string;
+    imgSrc: string;
+    placeholder: string;
+    otherClasses?: string
+}
+
+const LocalSearch = ({ route,imgSrc,placeholder,otherClasses }: Props) => {
     return (
-        <div>
-            search 
+        <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}>
+        <Image
+            src={imgSrc}
+            width={24}
+            height={24}
+            alt='Search'
+            className='cursor-pointer' 
+        />
+        <Input type='text' placeholder={placeholder} className='paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none' /> 
         </div>
     );
 };
