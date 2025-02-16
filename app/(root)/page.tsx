c1b687bbfe3ce9f904e9a4048c93ee01e82cf95b
easylearningbd <elebdsoft@gmail.com>
2025-02-17 03:39:06 +0600

    n43 Local Search Bar Part 5

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,47 @@ import ROUTES from '@/constants/routes';
 import Link from 'next/link';
 import React from 'react';
 
-const Home = async () => { 
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
+
+// query=ariyan
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string}>
+}
+
+const Home = async ({searchParams}: SearchParams) => { 
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter(( question ) => 
+  question.title.toLowerCase().includes(query?.toLowerCase()) );
 
   return (
     <>
@@ -24,12 +64,12 @@ const Home = async () => {
       />
     </section>
 
-    Home Filter part 
+    {/* Home Filter part  */}
 
     <div className='mt-10 flex w-full flex-col gap-6'>
-      <h2>Qustions one </h2>
-      <h2>Qustions Two </h2>
-      <h2>Qustions Three </h2>
+      {filteredQuestions.map((question) => (
+        <h1 key={question._id}>{question.title}</h1>
+      ))}
     </div>
 
        
