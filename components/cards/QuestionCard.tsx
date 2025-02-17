7b0dc4deba518ef355aef48cf6663d0b6a18e438
easylearningbd <elebdsoft@gmail.com>
2025-02-18 04:53:27 +0600

    n50 Question Card Part 4

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -3,6 +3,7 @@ import { getTimeStamp } from '@/lib/utils';
 import Link from 'next/link';
 import React from 'react';
 import TagCard from './TagCard';
+import Metric from '../Metric';
 
 interface Props {
     question : Question;
@@ -31,11 +32,42 @@ const QuestionCard = ({ question: { _id,title,tags,author,upvotes,answers,views,
     </div>
 
     <div className='flex-between mt-6 w-full flex-wrap gap-3'>
+        <Metric
+            imgUrl={author.image}
+            alt={author.name}
+            value={author.name}
+            title={`- asked ${getTimeStamp(createdAt)} `}
+            href={ROUTES.PROFILE(author._id)}
+            textStyles='body-medium text-dark400_light700'
+            isAuthor        
+        />
 
-    </div>
+        <div className='flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start'> 
+          <Metric
+            imgUrl="/icons/like.svg"
+            alt="like"
+            value={upvotes}
+            title="Upvotes" 
+            textStyles='small-medium text-dark400_light800' 
+        />
 
+            <Metric
+            imgUrl="/icons/message.svg"
+            alt="answers"
+            value={answers}
+            title="Answers" 
+            textStyles='small-medium text-dark400_light800' 
+        />
 
-    
+            <Metric
+            imgUrl="/icons/eye.svg"
+            alt="views"
+            value={views}
+            title="Views" 
+            textStyles='small-medium text-dark400_light800' 
+        />
+        </div> 
+    </div> 
 </div>
     );
 };
