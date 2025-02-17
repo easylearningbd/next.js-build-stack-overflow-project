ee0585975499cb4d7a92db9cea13bdac4dc688cb
easylearningbd <elebdsoft@gmail.com>
2025-02-18 00:42:48 +0600

    n45 Filter Setup Part 2

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -3,6 +3,7 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useState } from 'react';
 import { Button } from '../ui/button';
 import { cn } from '@/lib/utils';
+import { formUrlQuery, removeKeysFromUrlQuery } from '@/lib/url';
 
 
 const filters = [
@@ -23,6 +24,27 @@ const HomeFilter = () => {
     const filterParams = searchParams.get("filter");
     const [active, setActive] = useState(filterParams || ""); 
 
+    const handleTypeClick = (filter: string) => {
+        let newUrl = "";
+
+        if (filter === active) {
+            setActive("");
+
+            newUrl = removeKeysFromUrlQuery({
+                params: searchParams.toString(),
+                keysToRemove: ["filter"],
+            }); 
+        } else {
+            setActive(filter);
+            newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: "filter",
+                value: filter.toLowerCase(),
+            });  
+        } 
+        router.push(newUrl, {scroll: false}); 
+    };
+
     return (
 <div className='mt-10 hidden flex-wrap gap-3 sm:flex'>
 {
@@ -32,7 +54,7 @@ const HomeFilter = () => {
             ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
             : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
  
-        )}>
+        )} onClick={() => handleTypeClick(filter.value)} >
             {filter.name} 
         </Button>
     ))
