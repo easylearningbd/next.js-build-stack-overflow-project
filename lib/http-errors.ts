0763c4300432f32212f1f09ee36b134e879f281e
easylearningbd <elebdsoft@gmail.com>
2025-02-24 04:16:14 +0600

    n72 Implement Error Handling Part 2

--- lib/http-errors.ts
+++ lib/http-errors.ts
@@ -14,6 +14,30 @@ export class RequestError extends Error {
     }
 }
 
+export class ValidationError extends RequestError {
+    constructor(fieldErrors: Record<string, string[]>) {
+        const message = ValidationError.formatFieldErrors(fieldErrors);
+        super(400, message, fieldErrors);
+        this.name = "ValidationError";
+        this.errors = fieldErrors;
+    }
+
+    static formatFieldErrors(errors: Record<string, string[]>) : string {
+        const formattedMessages = Object.entries(errors).map(([field, messages]) => {
+            const fieldName = field.charAt(0).toUpperCase() + field.slice(1);
+    
+            if (messages[0] === "Required") {
+                return `${fieldName} is required`;
+            } else {
+                return messages.join(" and ")
+            }
+        });
+
+        return formattedMessages.join(" , ");
+    }  
+}
+ 
+
 
 export class NotFoundError extends RequestError {
     constructor(resource: string){
