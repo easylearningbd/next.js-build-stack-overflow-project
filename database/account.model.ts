5e634e1cee95ccf41f0133f5fed2c9ca4a39a51c
easylearningbd <elebdsoft@gmail.com>
2025-02-26 03:09:54 +0600

    n84 Create & Read Account Routes

--- database/account.model.ts
+++ database/account.model.ts
@@ -8,7 +8,7 @@ export interface IAccount {
     provider: string; // 'github' , 'google' ,'credentials'
     providerAccountId?: string;
 }
-
+ 
 export interface IAccountDoc extends IAccount, Document {}
 const AccountSchema = new Schema<IAccount>(
     {
