855f6791ef564fcf811cdc134b5596fc5112ad77
easylearningbd <elebdsoft@gmail.com>
2025-02-27 23:13:15 +0600

    n92 Accounts for oAuth Part 3

---
+++ types/action.d.ts
@@ -0,0 +1,10 @@
+interface SignInWithOAuthParams {
+    provider: "github";
+    providerAccountId: string;
+    user: {
+        email: string;
+        name: string;
+        image: string;
+        username: string;
+    }
+}
