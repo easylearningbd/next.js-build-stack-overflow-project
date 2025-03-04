ad4ee38570fa6a0e6c4de572cc2eab0184aebb11
easylearningbd <elebdsoft@gmail.com>
2025-03-05 03:42:37 +0600

    n103 Sign Up Part 2

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,13 +1,18 @@
 "use server";
 
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { SignInSchema } from "../validations";
+import { SignUpSchema } from "../validations";
+import User from "@/database/user.model";
+import Account from "@/database/account.model";
+import { signIn } from "@/auth";
 
 
 export async function signUpWithCredentials( params: AuthCredentials) : Promise<ActionResponse> {
-    const validationResult = await action({ params, schema: SignInSchema});
+    const validationResult = await action({ params, schema: SignUpSchema});
 
     if (validationResult instanceof Error) {
         return handleError(validationResult) as ErrorResponse;
@@ -18,5 +23,45 @@ export async function signUpWithCredentials( params: AuthCredentials) : Promise<
     const session = await mongoose.startSession();
     session.startTransaction(); 
 
+    try {
+        const existingUser = await User.findOne({ email }).session(session);
+
+        if (existingUser) {
+            throw new Error("User already exists");
+        }
+
+        const existingUsername = await User.findOne({ username }).session(session);
+
+        if (existingUsername) {
+            throw new Error("Username already exists");
+        }
+     
+    const hashedPassword = await bcrypt.hash(password,12);
+    const [newUser] = await User.create([{ username,name,email }], {session});
+
+    await Account.create(
+        [
+            {
+                userId: newUser._id ,
+                name,
+                provider: "credentials",
+                providerAccountId: email,
+                password: hashedPassword, 
+            }
+        ],
+        {session}
+    );
+
+    await session.commitTransaction();
+    await signIn("credentials", { email, password, redirect: false });
+
+    return { success: true};
+
+    } catch (error) {
+        await session.abortTransaction();
+        return handleError(error) as ErrorResponse;
+    } finally {
+        await session.endSession();
+    } 
 
 }
