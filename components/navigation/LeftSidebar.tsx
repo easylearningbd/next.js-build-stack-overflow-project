6a1097b03e8ef84e7922e4a403668434675cbc83
easylearningbd <elebdsoft@gmail.com>
2025-03-06 00:40:40 +0600

    n107 Auth Checks and Logout Part 1

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -4,8 +4,13 @@ import { Button } from '../ui/button';
 import Link from 'next/link';
 import ROUTES from '@/constants/routes';
 import Image from 'next/image';
+import { auth, signOut } from '@/auth';
+import { LogOut } from 'lucide-react';
 
-const LeftSidebar = () => {
+const LeftSidebar = async () => {
+    const session = await auth();
+    const userId = session?.user?.id; 
+
     return (
 <section className='custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'>
     <div className='flex flex-1 flex-col gap-6'>
@@ -13,6 +18,19 @@ const LeftSidebar = () => {
     </div>
 
     <div className='flex flex-col gap-3'>
+    {
+        userId ? (
+            <form action={async () => {
+                "use server";
+                await signOut();
+            }}>
+             <Button type='submit' className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
+            <LogOut className='size-5 text-black dark:text-white' />
+            <span className='text-dark300_light900 max-lg:hidden'>Logout</span>
+                </Button> 
+            </form>
+        ) :
+        <>
         <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
             <Link href={ROUTES.SIGN_IN}>
             <Image
@@ -38,7 +56,9 @@ const LeftSidebar = () => {
                 />
                 <span className='max-lg:hidden'>Sign Up  </span>
             </Link> 
-        </Button>
+        </Button> 
+        </>
+    } 
 
     </div>
     
