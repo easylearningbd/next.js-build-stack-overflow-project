67a533a8da0220b50f9d23d541cd5f6b927f03bc
easylearningbd <elebdsoft@gmail.com>
2025-03-08 05:36:35 +0600

    n126 Fetch and Display Questions Part 6

--- components/DataRenderer.tsx
+++ components/DataRenderer.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { Button } from './ui/button';
 
 
 interface Props<T>{
@@ -34,8 +37,43 @@ interface StateSkeletonProps {
 }
 
 const StateSkeleton = ({
-    
-})
+    image,
+    title,
+    message,
+    button 
+}: StateSkeletonProps ) => (
+
+    <div className='mt-16 flex w-full flex-col items-center justify-center sm:mt-36'>
+        <>
+            <Image
+                src={image.dark}
+                alt={image.alt}
+                width={270}
+                height={200}
+                className='hidden object-contain dark:block' 
+            />
+            <Image
+                src={image.light}
+                alt={image.alt}
+                width={270}
+                height={200}
+                className='block object-contain dark:hidden' 
+            />
+        </>
+    <h2 className='h2-bold text-dark200_light900 mt-8'>{title}</h2>
+    <p className='body-regular text-dark500_light700 my-3.5 max-w-md text-center'>{message}</p>
+
+    {button && (
+        <Link href={button.href}>
+            <Button className='paragraph-medium min-h-[45px] rounded-lg bg-primary-500 px-4 py-3 text-light-900 hover:bg-primary-500'>
+                {button.text}
+            </Button>
+        
+        </Link>
+    )} 
+    </div> 
+
+)
 
 
 const DataRenderer = () => {
