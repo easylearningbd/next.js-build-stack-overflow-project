47669adf5bd2aa45e30b6aa5a2d69fa785b5bf54
easylearningbd <elebdsoft@gmail.com>
2025-03-09 05:32:18 +0600

    n135 Fixed Bugs For Add Tags for Question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -42,7 +42,7 @@ export async function createQuestion(
     for(const tag of tags) {
         const existingTag = await Tag.findOneAndUpdate(
             { name: { $regex: new RegExp(`^${tag}$`,"i") }},
-            { $setOnInsert: {name: tag}, $inc: { question: 1 } },
+            { $setOnInsert: {name: tag}, $inc: { questions: 1 } },
             { upsert: true, new: true, session }
         );
 
