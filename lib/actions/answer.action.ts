d1220dd12f094fe20e2cc337c906da7c96e75eed
easylearningbd <elebdsoft@gmail.com>
2025-03-11 03:26:50 +0600

    n148 Create Answer Action Part 2

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,10 +1,13 @@
 "use server"
 
 import mongoose from "mongoose";
-import { IAnswerDoc } from "@/database/answer.model";
+import Answer, { IAnswerDoc } from "@/database/answer.model";
 import action from "../handlers/action";
 import { AnswerServerSchema } from "../validations";
 import handleError from "../handlers/error";
+import { Question } from "@/database";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
 
 export async function createAnswer( params: CreateAnswerParams) : Promise<ActionResponse<IAnswerDoc>> {
 
@@ -25,9 +28,37 @@ export async function createAnswer( params: CreateAnswerParams) : Promise<Action
     session.startTransaction();
 
     try {
-        
+        const question = await Question.findById(questionId);
+        if(!question) throw new Error("Question not found");
+
+        const [newAnswer] = await Answer.create(
+            [
+                {
+                    author: userId,
+                    question: questionId,
+                    content,
+                }
+            ],
+            { session }
+        );
+
+        if (!newAnswer) throw new Error("Failed to create answer");
+ 
+        question.answers += 1;
+        await question.save({ session });
+        await session.commitTransaction();
+
+        revalidatePath(ROUTES.QUESTION(questionId));
+
+        return {
+            success: true,
+            data: JSON.parse(JSON.stringify(newAnswer)),
+        }; 
     } catch (error) {
-        
-    } 
+        await session.abortTransaction();
+        return handleError(error) as ErrorResponse;
+    } finally {
+        await session.endSession();
+    }
 
 }
