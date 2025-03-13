0bc575f071f5223370c930f470dcfcb0e927607e
easylearningbd <elebdsoft@gmail.com>
2025-03-14 02:06:58 +0600

    n163 BugFix Integrate Actions on UI Part 3

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -22,7 +22,7 @@ export async function updateVoteCount( params: UpdateVoteCountParams, session?: 
     const { targetId, targetType, voteType,change } = validationResult.params!;
 
     const Model = targetType === "question" ? Question : Answer;
-    const voteField = voteType === "upvote" ? "upvote" : "downvote";
+    const voteField = voteType === "upvote" ? "upvotes" : "downvotes";
 
     try {
         const result = await Model.findByIdAndUpdate(
