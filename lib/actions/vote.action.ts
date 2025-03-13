692bcacea007589f80e476445c148f015d6a063a
easylearningbd <elebdsoft@gmail.com>
2025-03-13 21:25:43 +0600

    n162 BugFix Integrate Actions on UI Part 2

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -4,6 +4,8 @@ import action from "../handlers/action"
 import handleError from "../handlers/error";
 import { CreateVoteSchema, HasVotedSchema, UpdateVoteCountSchema } from "../validations"
 import { Answer, Question, Vote } from "@/database";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
 
 
 export async function updateVoteCount( params: UpdateVoteCountParams, session?: ClientSession) : Promise<ActionResponse> {
@@ -86,13 +88,23 @@ export async function createVote( params: CreateVoteParams)
                 {new: true, session}
             );
             await updateVoteCount(
+                { targetId, targetType, voteType:existingVote.voteType, change: -1},
+                session
+            );
+
+            await updateVoteCount(
                 { targetId, targetType, voteType, change: 1},
                 session
             );
         }
     } else {
         // if the user has not voted yet, create a new vote
-        await Vote.create([{ targetId, targetType, voteType, change: 1}],{session});
+        await Vote.create([{
+            author: userId,
+            actionId:targetId ,
+            actionType:targetType,
+            voteType 
+            }],{session});
         await updateVoteCount(
             {targetId, targetType, voteType, change: 1},
             session
@@ -100,6 +112,9 @@ export async function createVote( params: CreateVoteParams)
     }
     await session.commitTransaction();
     session.endSession();
+
+    revalidatePath(ROUTES.QUESTION(targetId)); 
+
     return {success: true};
         
     } catch (error) {
