8d0ba692d3d7f0100b26a653b0a24a289a0f873b
easylearningbd <elebdsoft@gmail.com>
2025-03-17 01:44:28 +0600

    n166 Add to Collection Page Part 2

--- lib/actions/collection.action.ts
+++ lib/actions/collection.action.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { Collection, Question } from "@/database";
 import action from "../handlers/action";
 import handleError from "../handlers/error";
 import { CollectionBaseSchema } from "../validations";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
 
 export async function toggleSaveQuestion( params: CollectionBaseParams) : Promise<ActionResponse<{ saved: boolean }>>{
 
@@ -20,12 +23,40 @@ export async function toggleSaveQuestion( params: CollectionBaseParams) : Promis
     const userId = validationResult.session?.user?.id;
 
     try {
+        const question = await Question.findById(questionId);
+        if (!question) throw new Error("Question not found");
 
-        
-        
-    } catch (error) {
-        return handleError(error) as ErrorResponse;
+        const collection = await Collection.findOne({
+            question: questionId,
+            author: userId
+        });
+
+    if (collection) {
+        await Collection.findByIdAndDelete(collection._id);
+
+        revalidatePath(ROUTES.QUESTION(questionId));
+        return {
+            success: true,
+            data: {
+                saved: false,
+            },
+        };
     }
 
+    await Collection.create({
+        question: questionId,
+        author: userId
+    });
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+        success: true,
+        data: {
+            saved: true,
+        },
+    }; 
+    } catch (error) {
+        return handleError(error) as ErrorResponse;
+    } 
 
 }
