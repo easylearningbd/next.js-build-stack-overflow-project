f3ac2b77c7bc5940ce8ef64615c728330f60e367
easylearningbd <elebdsoft@gmail.com>
2025-03-18 03:03:02 +0600

    n176 Create Filter Component Part 2

--- components/filters/CommonFilter.tsx
+++ components/filters/CommonFilter.tsx
@@ -11,6 +11,8 @@ import {
     SelectValue,
   } from "@/components/ui/select"
 import { useRouter, useSearchParams } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { formUrlQuery } from '@/lib/url';
 
   interface Filter {
     name: string;
@@ -30,11 +32,45 @@ const CommonFilter = ({filters,otherClasses="",containerClasses="" } : Props) =>
 
     const paramsFilter = searchParams.get("filter");
 
+    const handleUpdateParams = ( value: string) => {
+        const newUrl = formUrlQuery({
+            params: searchParams.toString(),
+            key: "filter",
+            value,
+        });
+        router.push(newUrl,{scroll:false})
+    }
+
 
     return (
-        <div>
-            
-        </div>
+<div className={cn("relative", containerClasses)}>
+    <Select
+    onValueChange={handleUpdateParams}
+    defaultValue={paramsFilter || undefined}
+    >
+        <SelectTrigger className={cn("body-regular no-focus light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5" ,otherClasses )} aria-label='Filter options'>
+
+        <div className='line-clamp-1 flex-1 text-left'>
+            <SelectValue placeholder="Select a filter" />
+        </div> 
+        </SelectTrigger>
+
+    <SelectContent>
+        <SelectGroup>
+            { filters.map((item) => (
+                <SelectItem key={item.value} value={item.value}>
+                    {item.name}
+                </SelectItem>
+            ))}
+        </SelectGroup>
+
+    </SelectContent>
+    
+
+
+    </Select>
+    
+</div>
     );
 };
 
