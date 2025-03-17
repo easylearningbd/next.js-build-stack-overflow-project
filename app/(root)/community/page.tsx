b1c5e0fbd9f5c16f4693f4ad1d95ccf11b74948d
easylearningbd <elebdsoft@gmail.com>
2025-03-18 01:31:41 +0600

    n173 Display All Users Part 1

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,9 +1,52 @@
+import UserCard from '@/components/cards/UserCard';
+import DataRenderer from '@/components/DataRenderer';
+import LocalSearch from '@/components/search/LocalSearch';
+import ROUTES from '@/constants/routes';
+import { EMPTY_USERS } from '@/constants/states';
+import { getUsers } from '@/lib/actions/user.action';
 import React from 'react';
 
-const Community = () => {
+const Community = async ({ searchParams }: RouteParams) => {
+    const { page, pageSize, query, filter } = await searchParams;
+
+    const { success, data, error } = await getUsers({
+        page: Number(page) || 1,
+        pageSize: Number(pageSize) || 10,
+        query,
+        filter
+    });
+
+    const { users } = data || {};
+   // console.log(users);
+
     return (
         <div>
-            <h1>Community</h1>
+            <h1 className='h1-bold text-dark100_light900'>All Users</h1>
+
+     <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+      <LocalSearch
+        route={ROUTES.COMMUNITY}
+        imgSrc="/icons/search.svg"
+        placeholder="Search questions..." 
+        otherClasses="flex-1"      
+      />
+    </div>
+
+    <DataRenderer
+      success={success}
+      error={error}
+      data={users}
+      empty={EMPTY_USERS}
+      render={(users) => (
+        <div className='mt-10 flex w-full flex-col gap-6'>
+        {users.map((user) => ( 
+          <UserCard key={user._id} {...user} />
+        ))}
+      </div>
+      )} 
+     />  
+
+
         </div>
     );
 };
--- constants/states.ts
+++ constants/states.ts
@@ -49,3 +49,8 @@ export const EMPTY_COLLECTIONS = {
         href: ROUTES.COLLECTION
     },
 };
+
+export const EMPTY_USERS = {
+    title: "No Users Found",
+    message: "You are not along. The only one here. more user are coming soon", 
+};
