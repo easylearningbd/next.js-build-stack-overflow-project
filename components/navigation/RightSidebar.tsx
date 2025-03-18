a3f632074daf655c66796a2dec8badf9a766824e
easylearningbd <elebdsoft@gmail.com>
2025-03-19 00:34:37 +0600

    n180 Show Top Questions

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -3,15 +3,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import TagCard from '../cards/TagCard';
-
-const hotQuestions = [
-    { _id: "1", title: "How to create a custom hook in react?"},
-    { _id: "2", title: "How to use React Query?"},
-    { _id: "3", title: "How to use Redux?"},
-    { _id: "4", title: "How to use React Router?"},
-    { _id: "5", title: "How to use Next Navigation?"},
-];
-
+import { getHotQuestions } from '@/lib/actions/question.action';
+import DataRenderer from '../DataRenderer';
+ 
 const popularTags = [
     { _id: "1", name: "react", questions: 100 },
     { _id: "2", name: "javascript", questions: 50 },
@@ -20,17 +14,30 @@ const popularTags = [
     { _id: "5", name: "react-query", questions: 20 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+    const { success, data: hotQuestions, error} = await getHotQuestions();
+
+
     return (
 <section className='pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-1 p-6 shadow-light-300 dark:shadow-none max-xl:hidden'>
     <div>
         <h3 className='h3-bold text-dark200_light_900'>Top Questions</h3>
     <div className='mt-7 flex w-full flex-col gap-[30px]'>
-        {
-          hotQuestions.map(( { _id,title }) => (
+        
+        <DataRenderer
+            data={hotQuestions}
+            empty={{
+                title: "No Question Found",
+                message: "No question have been asked yet"
+            }}
+            success={success}
+            error={error}
+            render={(hotQuestions ) => (
+                <div className='mt-7 flex w-full flex-col gap-[30px]'>
+            { hotQuestions.map(( { _id,title }) => (
             <Link 
                 key={_id}
-                href={ROUTES.PROFILE(_id)}
+                href={ROUTES.QUESTION(_id)}
                 className='flex cursor-pointer items-center justify-between gap-7' >
             <p className='body-medium text-dark500_light700'>
                 {title}
@@ -41,11 +48,14 @@ const RightSidebar = () => {
             width={20}
             height={20}
             className='invert-colors' 
-        />  
-
+        />   
             </Link>
           ))  
         } 
+                </div> 
+            )}  
+        /> 
+        
     </div> 
     </div>
 
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -6,6 +6,7 @@ import handleError from "../handlers/error";
 import mongoose, { FilterQuery } from 'mongoose';
 import Tag, { ITagDoc } from "@/database/tag.model";
 import TagQuestion from "@/database/tag-question.model"; 
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
     params: CreateQuestionParams
@@ -309,5 +310,24 @@ export async function incrementViews( params: IncrementViewsParams) : Promise<Ac
 
     } catch (error) {
         return handleError(error) as ErrorResponse
+    }
+}
+
+
+export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
+    try {
+        await dbConnect();
+
+        const questions = await Question.find()
+            .sort({ views: -1, upvotes: -1 })
+            .limit(5);
+
+        return {
+            success: true,
+            data: JSON.parse(JSON.stringify(questions)),
+        };
+
+    } catch (error) {
+        return handleError(error) as ErrorResponse;
     }
 }
