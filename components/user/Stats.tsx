d65557c0e73c6186435b8c10b9be26c22f88664a
easylearningbd <elebdsoft@gmail.com>
2025-03-19 03:05:53 +0600

    n186 Develop Stats UI Part 2

--- components/user/Stats.tsx
+++ components/user/Stats.tsx
@@ -1,3 +1,4 @@
+import { formatNumber } from '@/lib/utils';
 import Image from 'next/image';
 import React from 'react';
 
@@ -31,6 +32,22 @@ const Stats = ({ totalQuestions, totalAnswers,badges }: Props) => {
 <div className='mt-3'>
     <h4 className='h3-semibold text-dark200_light900'>Stats</h4>
     <div className='mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4'>
+
+    <div className='light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gal-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200'>
+        <div>
+            <p className='paragraph-bold text-dark200_light900'>
+                {formatNumber(totalQuestions)}
+            </p>
+            <p className='body-medium text-dark400_light700'>Questions </p>
+        </div>
+
+        <div>
+            <p className='paragraph-bold text-dark200_light900'>
+                {formatNumber(totalAnswers)}
+            </p>
+            <p className='body-medium text-dark400_light700'>Answers </p>
+        </div> 
+    </div>
 
 
    <StartsCard
