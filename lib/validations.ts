3e44dd18e50d2aa805e26fe3cb79f5f5ecf20d1a
easylearningbd <elebdsoft@gmail.com>
2025-03-19 05:04:21 +0600

    n188 Implement User Questions Tab Part 1

--- lib/validations.ts
+++ lib/validations.ts
@@ -191,6 +191,10 @@ export const GetUserSchema = z.object({
   userId: z.string().min(1, { message: "User ID is required"}),
 });
 
+export const GetUserQuestionsSchema = PaginatedSearchParamsSchema.extend({
+  userId: z.string().min(1, { message: "User ID is required"}),
+});
+
 
 
 
